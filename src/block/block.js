e812ae52518d83e7ef42cf26861f0702e5e6f003
JR Tashjian <jrtashjian@gmail.com>
2018-09-04 23:42:42 -0400

    Moving the Node React component into it's own file.

--- src/block/block.js
+++ src/block/block.js
@@ -117,27 +117,6 @@ const haveHeadingsChanged = function( oldHeadings, newHeadings ) {
 	return ! ! +changedHeadings.length;
 };
 
-class Node extends React.Component {
-	render() {
-		let childnodes = null;
-
-		if ( this.props.children ) {
-			childnodes = this.props.children.map( function( childnode ) {
-				return (
-					<Node key={ childnode.block.anchor } node={ childnode.block } children={ childnode.children } />
-				);
-			} );
-		}
-
-		return (
-			<li key={ this.props.node.anchor }>
-				<a href={ this.props.node.anchor } data-level={ this.props.node.level }>{ this.props.node.content }</a>
-				{ childnodes ? <ul>{ childnodes }</ul> : null }
-			</li>
-		);
-	}
-}
-
 /**
  * Register our Gutenberg block.
  */
---
+++ src/block/components/Node.js
@@ -0,0 +1,22 @@
+import { Component } from 'react';
+
+export default class Node extends Component {
+	render() {
+		let childnodes = null;
+
+		if ( this.props.children ) {
+			childnodes = this.props.children.map( function( childnode ) {
+				return (
+					<Node key={ childnode.block.anchor } node={ childnode.block } children={ childnode.children } />
+				);
+			} );
+		}
+
+		return (
+			<li key={ this.props.node.anchor }>
+				<a href={ this.props.node.anchor } data-level={ this.props.node.level }>{ this.props.node.content }</a>
+				{ childnodes ? <ul>{ childnodes }</ul> : null }
+			</li>
+		);
+	}
+}
