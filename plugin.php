817f0d95c005611fbca5f666934eeb27475c2764
JR Tashjian <jrtashjian@gmail.com>
2018-09-05 01:48:48 -0400

    Setting up WP_GitHub_Updater
    Closes #7

--- plugin.php
+++ plugin.php
@@ -21,3 +21,29 @@ if ( ! defined( 'ABSPATH' ) ) {
  * Block Initializer.
  */
 require_once plugin_dir_path( __FILE__ ) . 'src/init.php';
+
+/**
+ * GitHub Plugin Updater.
+ */
+function sbb_github_plugin_updater_test_init() {
+	include_once 'updater.php';
+
+	// define( 'WP_GITHUB_FORCE_UPDATE', true );
+
+	if ( is_admin() ) {
+		$config = array(
+			'slug'               => plugin_basename( __FILE__ ),
+			'proper_folder_name' => 'sbb-guidepost',
+			'api_url'            => 'https://api.github.com/repos/sortabrilliant/guidepost',
+			'raw_url'            => 'https://raw.github.com/sortabrilliant/guidepost/master',
+			'github_url'         => 'https://github.com/sortabrilliant/guidepost',
+			'zip_url'            => 'https://github.com/sortabrilliant/guidepost/archive/master.zip',
+			'requires'           => '4.9.8',
+			'tested'             => '4.9.8',
+			'readme'             => 'README.md',
+		);
+
+		new WP_GitHub_Updater( $config );
+	}
+}
+add_action( 'init', 'sbb_github_plugin_updater_test_init' );
