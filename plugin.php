dab09211faf2e68027052200b2b82d8071957dd6
JR Tashjian <jrtashjian@gmail.com>
2018-09-05 22:16:03 -0400

    Testing the Github plugin updater.

--- plugin.php
+++ plugin.php
@@ -5,7 +5,7 @@
  * Description: Add a list of internal links allowing your readers to quickly navigate around.
  * Author: Sorta Brilliant
  * Author URI: https://sortabrilliant.com
- * Version: 0.1.0
+ * Version: 0.1.1
  * License: GPL2+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  *
