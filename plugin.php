f28b586aa7bf41bed00c4f76b91898cb36de6dfe
JR Tashjian <jrtashjian@gmail.com>
2018-09-13 16:03:48 -0400

    Bumping version to 0.1.2

--- plugin.php
+++ plugin.php
@@ -5,7 +5,7 @@
  * Description: Add a list of internal links allowing your readers to quickly navigate around.
  * Author: sorta brilliant
  * Author URI: https://sortabrilliant.com
- * Version: 0.1.1
+ * Version: 0.1.2
  * License: GPL2+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  *
