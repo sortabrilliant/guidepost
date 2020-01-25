d48e36570fe5a10b75e0915bc57a4b3afddd0187
JR Tashjian <jrtashjian@gmail.com>
2020-01-25 18:55:36 -0500

    Update plugin slug

--- guidepost.php
+++ guidepost.php
@@ -14,78 +14,78 @@
 
 defined( 'ABSPATH' ) || exit;
 
-define( 'SBBGP_VERSION', '1.0.4' );
-define( 'SBBGP_PLUGIN_DIR', dirname( __FILE__ ) );
-define( 'SBBGP_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+define( 'GUIDEPOST_VERSION', '1.0.4' );
+define( 'GUIDEPOST_PLUGIN_DIR', dirname( __FILE__ ) );
+define( 'GUIDEPOST_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 
 /**
  * Registers all block assets so that they can be enqueued through Gutenberg in
  * the corresponding context.
  */
-function sbb_guidepost_register_block() {
+function guidepost_register_block() {
 	$default_asset_file = array(
 		'dependencies' => array(),
-		'version'      => SBBGP_VERSION,
+		'version'      => GUIDEPOST_VERSION,
 	);
 
 	// Editor Script.
-	$asset_filepath = SBBGP_PLUGIN_DIR . '/build/index.asset.php';
+	$asset_filepath = GUIDEPOST_PLUGIN_DIR . '/build/index.asset.php';
 	$asset_file     = file_exists( $asset_filepath ) ? include $asset_filepath : $default_asset_file;
 
 	wp_register_script(
-		'sbb-guidepost-editor',
-		SBBGP_PLUGIN_URL . 'build/index.js',
+		'guidepost-editor',
+		GUIDEPOST_PLUGIN_URL . 'build/index.js',
 		$asset_file['dependencies'],
 		$asset_file['version'],
 		true // Enqueue script in the footer.
 	);
 
 	// Frontend Script.
-	$asset_filepath = SBBGP_PLUGIN_DIR . '/build/sbb-guidepost-theme.asset.php';
+	$asset_filepath = GUIDEPOST_PLUGIN_DIR . '/build/guidepost-theme.asset.php';
 	$asset_file     = file_exists( $asset_filepath ) ? include $asset_filepath : array(
 		'dependencies' => array(),
-		'version'      => SBBGP_VERSION,
+		'version'      => GUIDEPOST_VERSION,
 	);
 
 	wp_register_script(
-		'sbb-guidepost-frontend',
-		SBBGP_PLUGIN_URL . 'build/sbb-guidepost-theme.js',
+		'guidepost-frontend',
+		GUIDEPOST_PLUGIN_URL . 'build/guidepost-theme.js',
 		$asset_file['dependencies'],
 		$asset_file['version'],
 		true
 	);
 
 	// Editor Styles.
-	$asset_filepath = SBBGP_PLUGIN_DIR . '/build/guidepost-editor.asset.php';
+	$asset_filepath = GUIDEPOST_PLUGIN_DIR . '/build/guidepost-editor.asset.php';
 	$asset_file     = file_exists( $asset_filepath ) ? include $asset_filepath : $default_asset_file;
 
 	wp_register_style(
-		'sbb-guidepost-editor',
-		SBBGP_PLUGIN_URL . 'build/guidepost-editor.css',
+		'guidepost-editor',
+		GUIDEPOST_PLUGIN_URL . 'build/guidepost-editor.css',
 		array(),
 		$asset_file['version']
 	);
 
 	// Frontend Styles.
-	$asset_filepath = SBBGP_PLUGIN_DIR . '/build/guidepost-style.asset.php';
+	$asset_filepath = GUIDEPOST_PLUGIN_DIR . '/build/guidepost-style.asset.php';
 	$asset_file     = file_exists( $asset_filepath ) ? include $asset_filepath : $default_asset_file;
 
 	wp_register_style(
-		'sbb-guidepost-frontend',
-		SBBGP_PLUGIN_URL . 'build/guidepost-style.css',
+		'guidepost-frontend',
+		GUIDEPOST_PLUGIN_URL . 'build/guidepost-style.css',
 		array(),
 		$asset_file['version']
 	);
 
 	register_block_type(
-		'sbb/guidepost',
+		'sortabrilliant/guidepost',
 		array(
-			'editor_script' => 'sbb-guidepost-editor',
-			'editor_style'  => 'sbb-guidepost-editor',
-			'script'        => 'sbb-guidepost-frontend',
-			'style'         => 'sbb-guidepost-frontend',
+			'editor_script' => 'guidepost-editor',
+			'editor_style'  => 'guidepost-editor',
+			'script'        => 'guidepost-frontend',
+			'style'         => 'guidepost-frontend',
 		)
 	);
 }
 
-add_action( 'init', 'sbb_guidepost_register_block' );
+add_action( 'init', 'guidepost_register_block' );
--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const iconEl = createElement( 'svg', { width: 24, height: 24 },
 	createElement( 'path', { d: 'M21.71,11.29l-9-9c-0.39-0.39-1.02-0.39-1.41,0l-9,9c-0.39,0.39-0.39,1.02,0,1.41l9,9c0.39,0.39,1.02,0.39,1.41,0l9-9C22.1,12.32,22.1,11.69,21.71,11.29z M14,14.5V12h-4v2c0,0.55-0.45,1-1,1h0c-0.55,0-1-0.45-1-1v-3c0-0.55,0.45-1,1-1h5V7.5l3.15,3.15c0.2,0.2,0.2,0.51,0,0.71L14,14.5z' } )
 );
 
-registerBlockType( 'sbb/guidepost', {
+registerBlockType( 'sortabrilliant/guidepost', {
 	title: __( 'Guidepost' ),
 	description: __( 'Add a list of internal links allowing your readers to quickly navigate around.' ),
 	icon: iconEl,
